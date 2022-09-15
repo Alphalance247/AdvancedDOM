a9d0dcfa66ec4160e06030e7b7d64f1cd84264e1
Alphalance247 <al.moshood017@gmail.com>
2022-09-15 23:02:56 +0100

    nothing much to commot yet

---
+++ index.js
@@ -0,0 +1,27 @@
+var numberOfDrums = document.querySelectorAll(".drum").length;
+var i = 0;
+
+
+function clickListener(){
+    alert("i got clicked");
+    }
+    while( i < numberOfDrums){ 
+    document.querySelectorAll(".drum")[i].addEventListener("click", clickListener);
+    i++;
+    };
+
+// var number = [];
+// var i = 0
+// function clickListener(){
+
+// while (i < numberOfDrums){
+//     document.querySelectorAll(".drum")[i].addEventListener("click", clickListener);
+    // document.querySelectorAll(".drum")[i].classList.add("added-class")
+    // console.log(document.querySelectorAll(".drum")[i])
+    // i++
+// }
+// alert("i got clicked");
+// }
+
+// clickListener()
+
