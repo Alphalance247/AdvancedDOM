d790830e3add2cd83d8e5eef8d1c0c1fe0368741
Alphalance247 <al.moshood017@gmail.com>
2022-09-16 12:32:26 +0100

    adding event listener

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var numberOfDrums = document.querySelectorAll(".drum").length;
-var i = 0;
+// var numberOfDrums = document.querySelectorAll(".drum").length;
+// var i = 0;
+// function clickListener(){
+//     alert("i got clicked");
+//     }
+//     while( i < numberOfDrums){ 
+//     document.querySelectorAll(".drum")[i].addEventListener("click", clickListener);
+//     i++;
+//     };
 
 
-function clickListener(){
-    alert("i got clicked");
-    }
-    while( i < numberOfDrums){ 
-    document.querySelectorAll(".drum")[i].addEventListener("click", clickListener);
-    i++;
-    };
 
 // var number = [];
 // var i = 0
@@ -24,4 +24,31 @@ function clickListener(){
 // }
 
 // clickListener()
+
+
+var allClick = document.querySelectorAll("button").length;
+
+for(var i = 0; i < allClick; i++){
+document.querySelectorAll("button")[i].addEventListener("click",eventListener)
+};
+function eventListener() {
+    alert("i got cliced");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
