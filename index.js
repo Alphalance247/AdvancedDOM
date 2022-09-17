7be3bb0cee8958dba8004ea7166c4ced66f38e45
Alphalance247 <al.moshood017@gmail.com>
2022-09-17 21:45:28 +0100

    adding event listener

--- index.js
+++ index.js
@@ -26,13 +26,44 @@
 // clickListener()
 
 
-var allClick = document.querySelectorAll("button").length;
+var allClick = document.querySelectorAll(".drum").length;
 
 for(var i = 0; i < allClick; i++){
-document.querySelectorAll("button")[i].addEventListener("click",eventListener)
+document.querySelectorAll(".drum")[i].addEventListener("click",eventListener)
 };
 function eventListener() {
-    alert("i got cliced");
+     var innerButton = this.innerHTML; 
+    switch (innerButton) {
+        case "w":
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+            break;
+            case "a":
+                var audio = new Audio('sounds/tom-2.mp3');
+                audio.play();
+            break;
+            case "s":
+                var audio = new Audio('sounds/tom-3.mp3');
+                audio.play();
+            break;
+            case "d":
+                var audio = new Audio('sounds/tom-4.mp3');
+                audio.play();
+            break;
+            case "j":
+                var audio = new Audio('sounds/snare.mp3');
+                audio.play();
+            break;
+            case "k":
+                var audio = new Audio('sounds/crash.mp3');
+                audio.play();
+            break;
+            case "l":
+                var audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();
+        default:
+            break;
+    }
 }
 
 
